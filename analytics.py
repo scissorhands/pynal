6879775720ff5409bbbe76d12f0e7022fe071989
Edward Scissorhands <edward_beats@hotmail.com>
2017-07-27 13:17:17 -0500

    running example

---
+++ analytics.py
@@ -0,0 +1,73 @@
+import config
+import argparse
+
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+import httplib2
+from oauth2client import client
+from oauth2client import file
+from oauth2client import tools
+
+import json
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+DISCOVERY_URI = ('https://analyticsreporting.googleapis.com/$discovery/rest')
+KEY_FILE_LOCATION = config.key_file_location
+SERVICE_ACCOUNT_EMAIL = config.service_account_email
+VIEW_ID = config.view_id
+
+
+def initialize_analyticsreporting():
+  credentials = ServiceAccountCredentials.from_p12_keyfile(
+    SERVICE_ACCOUNT_EMAIL, KEY_FILE_LOCATION, scopes=SCOPES)
+  http = credentials.authorize(httplib2.Http())
+  analytics = build('analytics', 'v4', http=http, discoveryServiceUrl=DISCOVERY_URI)
+  return analytics
+
+
+def get_report(analytics):
+  return analytics.reports().batchGet(
+      body={
+        'reportRequests': [
+        {
+          'viewId': VIEW_ID,
+          'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
+          'metrics': [{'expression': 'ga:sessions'}]
+        }]
+      }
+  ).execute()
+
+
+def print_response(response):
+  for report in response.get('reports', []):
+    columnHeader = report.get('columnHeader', {})
+    dimensionHeaders = columnHeader.get('dimensions', [])
+    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+    rows = report.get('data', {}).get('rows', [])
+
+    for row in rows:
+      dimensions = row.get('dimensions', [])
+      dateRangeValues = row.get('metrics', [])
+
+      for header, dimension in zip(dimensionHeaders, dimensions):
+        print( header + ': ' + dimension)
+
+      for i, values in enumerate(dateRangeValues):
+        print( 'Date range (' + str(i) + ')')
+        for metricHeader, value in zip(metricHeaders, values.get('values')):
+          print( metricHeader.get('name') + ': ' + value)
+
+
+def main():
+  analytics = initialize_analyticsreporting()
+  try:
+    response = get_report(analytics)
+    print_response(response)
+  except Exception as e:
+    print(e)
+  finally:
+    print('done')
+
+if __name__ == '__main__':
+  main()
